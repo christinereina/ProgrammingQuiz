4a3af7d498f2a19c2f9a9759cd63b1fd1255066f
blacchoy@gmail.com <blacchoy@gmail.com>
2020-01-17 15:54:55 -0800

    updated my scripts.js file, currently debugging to ensure my quiz results will display

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,30 @@
 $(document).ready(function() {
-  $("form#quiz").submit(function(event) {
-    event.preventDefault();
 
-    var faveprogram = $("input:radio[name=program]:checked").val();
+  $("#quiz").submit(function(event) {
+    var question1 = parseInt($("input:radio[name=q1]:checked").val());
+    var question2 = parseInt($("input:radio[name=q2]:checked").val());
+    var question3 = parseInt($("input:radio[name=q3]:checked").val());
+    var question4 = parseInt($("input:radio[name=q4]:checked").val());
+    var question5 = parseInt($("input:radio[name=q5]:checked").val());
+    var result = question1 + question2 + question3 +  question4 + question5;
 
-  if ( faveprogram === "a") {
+    console.log(result)
+
+  if (result >= 7) {
       document.body.style.backgroundColor = "blue";
-  } else if (faveprogram === "b") {
+  } else if (result >= 5) {
     document.body.style.backgroundColor = "yellow";
-  } else if (faveprogram === "c") {
+  } else if (result >=3) {
     document.body.style.backgroundColor = "red";
   } else {
     document.body.style.backgroundColor = "black";
   }
 
-  $(document).ready(function() {
-    $(".clickable0").click(function() {
-      $(".element-hidden0").toggle();
-      $(".element-showing0").toggle();
-    });
-  });
+$(".clickable0").click(function() {
+  $(".element-hidden0").toggle();
+  $(".element-showing0").toggle();
 
-  });
+  event.preventDefault();
+});
+});
 });
